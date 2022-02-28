367278b18ae363e13fcc3f243883170e375a4602
devyosse <narutoshippuden884@gmail.com>
2022-02-28 09:43:01 +0100

    exo 206

--- index.php
+++ index.php
@@ -10,3 +10,55 @@
  *
  * ( PS: Sélectionnez, mais affichez le résultat à chaque fois ! ).
  */
+
+$server = 'localhost';
+$user = 'root';
+$pass = '';
+$db = "exo206";
+
+try{
+
+
+    $conn = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $request = $conn->prepare("SELECT * FROM user");
+
+    if ($request->execute()){
+        foreach ($request->fetchAll() as $value){
+            echo "<p>" . $value['username'] . "</p>";
+        }
+    }
+
+    $request = $conn->prepare("SELECT * FROM article");
+
+    if ($request->execute()){
+        foreach ($request->fetchAll() as $value){
+            echo "<p>" . $value['titre'] . "</p>";
+             echo "<p>" . $value['contenu'] . "</p>";
+        }
+    }
+
+    $request = $conn->prepare("SELECT * FROM user WHERE EXISTS (SELECT * FROM article WHERE article.user_fk = user.id)");
+    if ($request->execute()){
+        foreach ($request->fetchAll() as $value){
+            echo "<pre>";
+            print_r($value);
+            echo "</pre>";
+        }
+    }
+
+
+    $request = $conn->prepare("SELECT * FROM user WHERE EXISTS (SELECT * FROM article WHERE article.user_fk = user.id)");
+    if ($request->execute()){
+        foreach ($request->fetchAll() as $value){
+            echo "<pre>";
+            print_r($value);
+            echo "</pre>";
+        }
+    }
+}
+catch (PDOException $e){
+    die($e->getMessage());
+}
